8381a732ea5a0a352e96945b3bf5bc24bc23ddcf
victorchibuikem <okekevictor201303@gmail.com>
2023-03-30 10:50:52 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,31 +1,10 @@
-SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
-
-	SECTION .text
-	extern printf
-	global main
+     global    main
+          extern    printf
 main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-	mov eax, 0
-	ret
-
-Advanced task
-
-vi
-#include <stdio.h>
-
-/**
-* bmain - function exeucted before main
-* Return: no return.
-*/
-
-void _attribute_ ((constructor)) bmain()
-{
-	printf("You're beat! and yet, you must alllow");
-	Printf(",/nI bore my house upon my back!/n";
-}
